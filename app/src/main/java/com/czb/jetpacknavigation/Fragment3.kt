a468b643ce08ef84d6ed9a122322c38f2f6b6c78
pengchengfuGit <785961112@qq.com>
2019-08-13 15:59:59 +0800

    popUpTo and popUpToInclusive

--- app/src/main/java/com/czb/jetpacknavigation/Fragment3.kt
+++ app/src/main/java/com/czb/jetpacknavigation/Fragment3.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class Fragment3 : Fragment(){
+class Fragment3 : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment3, container, false)
+        var view = inflater.inflate(R.layout.fragment3, container, false)
+        return view
     }
 }
