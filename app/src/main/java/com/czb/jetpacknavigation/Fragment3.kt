698abc70723d4941824069811fca219c5b8d566f
pengchengfuGit <785961112@qq.com>
2019-08-14 10:15:47 +0800

    pop

--- app/src/main/java/com/czb/jetpacknavigation/Fragment3.kt
+++ app/src/main/java/com/czb/jetpacknavigation/Fragment3.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class Fragment3 : Fragment() {
     override fun onCreateView(
@@ -14,6 +15,9 @@ class Fragment3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment3, container, false)
+        view.findViewById<TextView>(R.id.tv3).setOnClickListener(View.OnClickListener {
+            findNavController().navigate(R.id.action_fragment3_to_fragment1)
+        })
         return view
     }
 }
